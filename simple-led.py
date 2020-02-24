8bb62e51593de67d78f23134389426cd7d8dea94
Niclas <61380272+xniclaserdx@users.noreply.github.com>
2020-02-24 18:29:05 +0100

    comments

--- simple-led.py
+++ simple-led.py
@@ -4,16 +4,16 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(13, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT) # replace 13 with the designated port
 
 lightLED(3)
 
 def lightLED(e):
     x=0
     while(x<e):
-        GPIO.output(13, GPIO.HIGH)
+        GPIO.output(13, GPIO.HIGH)  # replace 13 with the designated port
         time.sleep(.1)
-        GPIO.output(13, GPIO.LOW)
+        GPIO.output(13, GPIO.LOW)   # replace 13 with the designated port
         time.sleep(.1)
         x=+1
 
